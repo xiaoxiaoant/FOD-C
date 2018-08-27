5d6e9a5cffd816c6ad3b4193a6b71bfde2835ab5
Ant <xiaoxiaoant@hotmail.com>
2018-08-27 15:17:35 +0800

    [*]update ip and port

--- FutuOpenDClient/main.cpp
+++ FutuOpenDClient/main.cpp
@@ -1,4 +1,5 @@
 ﻿#include <iostream>
+#include <assert.h>
 #include "NetCenter.h"
 #include "QuoteHandler.h"
 
@@ -6,12 +7,19 @@ using namespace ftq;
 
 int main(int argc, const char * argv[])
 {
+    if (argc != 3)
+    {
+        fprintf(stderr, "usage: %s ip port\n", argv[0]);
+    }
+    
+    assert(argc == 3);
+
     NetCenter::instance()->init(uv_default_loop());
 
     IProtoHandler *quote_handler = new QuoteHandler();
     NetCenter::instance()->set_proto_handler(quote_handler);
 
-    NetCenter::instance()->connect("127.0.0.1", 11111);
+    NetCenter::instance()->connect(argv[1], atoi(argv[2]));
     uv_run(uv_default_loop(), UV_RUN_DEFAULT);
 
     return 0;
